2d2320f6fa1a4de3f7dc269059d353e56a7b67cb
Jean-Samuel <jscaron3@hotmail.com>
2026-05-20 16:42:20 -0400

    Update build-links.js

--- build-links.js
+++ build-links.js
@@ -10,17 +10,14 @@ const links = dirs
   .map(dir => `<a href="/Tools/tools/${dir}/">${dir}</a>`)
   .join("\n");
 
-const html = `
-<!doctype html>
+fs.writeFileSync(
+  "index.html",
+  `<!doctype html>
 <html>
-<head>
-  <meta charset="utf-8" />
-  <title>Tools</title>
-</head>
 <body>
-  ${links}
+${links}
 </body>
-</html>
-`;
+</html>`
+);
 
-fs.writeFileSync("./index.html", html);
+console.log("Generated links:", dirs);
