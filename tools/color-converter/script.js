47c7376439937fa072c0498f4a5aa2e334fe53d5
Jean-Samuel <jscaron3@hotmail.com>
2026-05-25 16:47:54 -0400

    Update

--- tools/color-converter/script.js
+++ tools/color-converter/script.js
@@ -1,4 +1,7 @@
 import { Pane } from "https://esm.sh/tweakpane@4.0.4";
+import { ensureToolShell } from "../tool-shell.js";
+
+ensureToolShell();
 
 jQuery(document).ready(function () {
   const config = {
