a0969c43d5807b3a2e00f4f53348ee4beba02536
Jean-Samuel <jscaron3@hotmail.com>
2026-05-26 13:03:18 -0400

    Update

--- tools/logos-resizer/script.js
+++ tools/logos-resizer/script.js
@@ -102,7 +102,7 @@ function createCard(item) {
 	card.innerHTML = `
         <div class="card-top">
             <div class="file-meta">
-                <div class="filename" contenteditable="true">${item.fileName}</div>
+                <div class="filename"><span contenteditable="true">${item.fileName}</span></div>
                 <div class="file-infos"><div class="filetype">${item.ext}</div>${ogSize}</div>
 								${rendered_sizes}			
             </div>
@@ -133,8 +133,8 @@ function createCard(item) {
                 Allow Upscale
             </label>
 						
-						<button type="button" data-fit="contain">Fit (contain)</button>
-						<button type="button" data-fit="cover">Fill (cover)</button>
+						<button type="button" data-fit="contain">Contain</button>
+						<button type="button" data-fit="cover">Cover</button>
 						
 						<span class="rotate-btn" title="Rotate"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#000" d="M17.65 6.35A7.96 7.96 0 0 0 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08A5.99 5.99 0 0 1 12 18c-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4z"/></svg></span>
         </div>
